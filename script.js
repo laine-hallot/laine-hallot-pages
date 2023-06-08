35d348a1cfcc95e404344481159003cee41d34a9
Laine Hallot <lainehallot@gmail.com>
2023-06-08 18:31:41 -0500

    added scroll effects and work section

--- script.js
+++ script.js
@@ -2,13 +2,49 @@ document.addEventListener('alpine:init', () => {
   Alpine.data('page', () => ({
     expanded: false,
     articleBodyScrollHeight: undefined,
+    percentageOfHeaderRemaining: 1,
+    percentageOfHeaderNotOnScreenString: '0%',
+    headerTransformString: `translateY(0%)`,
+
+    createTranslateYString(percentageString) {
+      `translateY(${percentageString})`;
+    },
 
     scrollToFirst() {
-      const firstSection = document.getElementById('first');
-      firstSection;
+      this.$event.preventDefault();
+      const firstSection = document.getElementById('employment');
+      this.$root.scrollTo({
+        left: undefined,
+        top: firstSection.offsetTop,
+        behavior: 'smooth',
+      });
     },
     handleScroll() {
-      //console.log($event);
+      if (window.innerWidth > 640) {
+        const yScrollPos = this.$event.target.scrollTop;
+
+        const bottomOfHeaderPos =
+          this.$refs.header.offsetTop + this.$refs.header.offsetHeight;
+
+        const pixelsRemaining = bottomOfHeaderPos - yScrollPos;
+
+        const percentage = Math.max(
+          pixelsRemaining / bottomOfHeaderPos,
+          0,
+        ).toFixed(2);
+        const percentageOfHeaderRemaining = parseFloat(percentage);
+
+        this.percentageOfHeaderRemaining = percentageOfHeaderRemaining;
+
+        const percentageOfHeaderNotOnScreenString = `${
+          (1 - percentageOfHeaderRemaining) * 100
+        }%`;
+
+        this.percentageOfHeaderNotOnScreenString =
+          percentageOfHeaderNotOnScreenString;
+
+        this.headerTransformString = `translateY(${percentageOfHeaderNotOnScreenString})`;
+      }
     },
   }));
 
